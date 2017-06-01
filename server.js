2a975a0fc4668c8fe887a65b8bf51abcae15804b
Dmitry Ryumkin <dima@Dmitrys-MacBook-Pro.local>
2017-06-01 12:18:51 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const cheerio = require("cheerio"),
+      req = require("tinyreq"),
+      curl = require('curlrequest'),
+      esprima = require('esprima'),
+      https = require('https'),
+      fs = require('fs'),
+      http = require('http')
+
+const cookie = //' paste here your userid=id ';
+
+const formatNumber = (digit) => {
+    return digit < 9 ? '0' + (digit + 1).toString() : (digit + 1).toString();
+}
+
+const loadListOfVideo = (address) => {
+    req(address, function (err, body) {
+    let $ = cheerio.load(body);
+    const comments = $('a.list-tray-item.icon-opacity-container');
+    launcher(comments);
+    
+});
+}
+
+const launcher = (comments) => {
+    for (var i=0; i < comments.length; i++) {
+        console.log(`${formatNumber(i)}-${comments[i].attribs['data-title']} - ${comments[i].attribs['data-film-id']}`);
+        getLinkToVideo(comments[i].attribs['data-film-id'], formatNumber(i), comments[i].attribs['data-title']);
+    };
+}
+
+const getLinkToVideo = (id, number, name) => {
+    const requestOptions = { url: `https://www.thegreatcoursesplus.com/embed/player?filmId=${id}`,
+                             headers: { Cookie: `${cookie}`}};
+    curl.request(requestOptions, (err, stdout, meta) => {
+        const parsedHTML = cheerio.load(stdout);
+        const script1 = parsedHTML('script')[6].children[0].data;
+        var arr = /.*9216kbps.*/.exec(script1);
+        const ss01 = /"(.*?)"/.exec(arr[0]);
+        const link = ss01[0].replace(/['"]+/g, '');
+        downloadVideo(link, number, name);
+    });
+}
+
+const downloadVideo = (link, number, name) => {
+    if (link.substr(4,1) === 's') {
+        var file = fs.createWriteStream(`${number}-${name}.mp4`);
+        var request = https.get(link, function(response) {
+            console.log(`Downloading file ${number}`);
+            response.pipe(file);
+        });
+    } else {
+        var file = fs.createWriteStream(`${number}-${name}.mp4`);
+        var request = http.get(link, function(response) {
+            console.log(`Downloading file ${number}`);
+            response.pipe(file);
+        });
+    }
+}
+
+
+loadListOfVideo("https://www.thegreatcoursesplus.com/yoga-for-a-healthy-mind-and-body");
+
